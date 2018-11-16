69382f06f20d411c9592712eb92cf4f6c49189b2
Nyla Khalil <nylakhalil@users.noreply.github.com>
2018-11-15 19:47:25 -0500

    Use new modules in Geosense

--- Geosense.py
+++ Geosense.py
@@ -1,37 +1,17 @@
 import os
+import logging
 
 import argparse
-import osr
-from osgeo import gdal
 
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
+import process.Raster as raster
+from config.Settings import LOG_FORMAT
 
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 script_dir = os.path.dirname(os.path.abspath(__file__))
-
-class GeoInfo():
-	def __init__(self, driver,crs, data, count, height, width):
-		self.driver = driver
-		self.crs = 'epsg:' + crs
-		self.data = data
-		self.count = count
-		self.height = height
-		self.width = width
-
-
-def process_raster(output_file, input_file, display_type):
-	options = gdal.DEMProcessingOptions(zeroForFlat=True)
-	dataset = gdal.DEMProcessing(output_file, input_file, display_type, options=options)
-	print("Dataset Processed: {}".format(dataset.GetDescription()))
-
-
-def read_raster(filepath):
-	dataset = gdal.Open(filepath)
-	proj = osr.SpatialReference(wkt=dataset.GetProjection())
-	crs = proj.GetAttrValue('AUTHORITY', 1)
-	return GeoInfo(dataset.GetDriver().LongName, crs, dataset.ReadAsArray(), dataset.RasterCount, dataset.RasterXSize, dataset.RasterYSize)
 
 
 def display_raster(geoinfo):
@@ -53,8 +33,8 @@ if __name__ == '__main__':
 
 	src_file = os.path.join(script_dir, args.srcfile)
 	out_file = os.path.join(script_dir, args.outfile)
-	print('Source File: {}, Out File: {}'.format(src_file, out_file))
+	logging.info('Source File: {}, Out File: {}'.format(src_file, out_file))
 
-	process_raster(out_file, src_file, args.process)
-	geoinfo = read_raster(out_file)
+	raster.process_raster(out_file, src_file, args.process)
+	geoinfo = raster.read_raster(out_file)
 	#display_raster(geoinfo)
